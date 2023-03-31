02d965b2f8a8b13fff93ced01637a6e269044c8c
TommiKabelitz <tommi.kabelitz@gmail.com>
2023-03-31 13:17:54 +1030

    Added check for num processes with npoints and formatted with black.

--- SimpleSummation.py
+++ SimpleSummation.py
@@ -10,25 +10,30 @@ size = comm.Get_size()
 
 # Define array to sum on the head process
 if rank == 0:
-    array_to_sum = np.linspace(0,100,101)
+    npoints = 100
+    if npoints % size != 0:
+        print("npoints must be divisible by number of processes")
+        comm.Abort()
+
+    array_to_sum = np.linspace(1, 100, npoints)
     print(f"Expected value of sum: {np.sum(array_to_sum)} \n")
 
     # Split array into even chunks
-    split_array = np.array_split(array_to_sum,size,axis = 0)
+    split_array = np.array_split(array_to_sum, size, axis=0)
 
 else:
     # Define variable name on other processes to prevent errors
     split_array = None
-    
+
 # Scatter the arrays across the processes
 # Yeah this isn't the "true" way to scatter an array but fight me
-local_array = comm.bcast(split_array, root = 0)[rank]
+local_array = comm.bcast(split_array, root=0)[rank]
 
 # Sum the array on the local process
 local_sum = np.sum(local_array)
 
 # Collect the sums, again, could be better, but refer above
-total_sum = comm.gather(local_sum, root = 0)
+total_sum = comm.gather(local_sum, root=0)
 
 if rank == 0:
     total_sum = np.sum(total_sum)
